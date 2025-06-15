7df1843db535e4167b3193b370066ecee6ce4964
Raian Emanuel Uchôa Laurentino <raian.pro9@gmail.com>
2025-06-15 05:43:44 -0300

    Commit inicial

---
+++ src/test/java/br/com/raian/meusgastos/MeusgastosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.raian.meusgastos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeusgastosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
